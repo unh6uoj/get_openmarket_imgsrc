c24ea9c4d3fcdd6955585d869d735152ce0d6861
unh6uoj <fpdlxms159@gmail.com>
2022-04-06 18:03:40 +0900

    update : add get detail v2

--- main.py
+++ main.py
@@ -35,9 +35,50 @@ def get_detail(url):
             elif img.attrs["class"][0] == "se-inline-image-resource":
                 r_text += f'<img src="{img.attrs["data-src"]}" />' + "\n"
     except:
-        pass
+        return False
 
     return r_text
+
+def get_detail_v2(url):
+    code = url.split("/")[5].split("?")[0]
+    merchant_code, etc_code = get_etc_no(url)
+
+    r = requests.get(
+        f"https://smartstore.naver.com/i/v1/products/{code}/contents/{merchant_code}/PC"
+    )
+
+    soup = BeautifulSoup(str(r.json()["renderContent"]), "html.parser")
+
+    r_text = "상세페이지\n"
+    try:
+        for img in soup.find_all("img"):
+            r_text += f'<img src="{img.attrs["data-src"]}" />' + "\n"
+    except Exception as e:
+        print(e)
+
+    return r_text
+
+# 기타 상품번호 추출
+def get_etc_no(url):
+    req = requests.get(url)
+    html = req.text
+    soup = BeautifulSoup(html, 'html.parser')
+    try:
+        # 받은 url에서 json추출
+        # salecount는 두 번째 script태그 안에 있다
+        script = soup.find_all('script')[1]
+        # 가져온 script를 str형으로 변환
+        script = str(script)
+        # str데이터 스플릿 (json형태로 변환)
+        script_json = script[script.find(
+            '__=')+3: script.find('</script>')]
+        # str을 dictionary로 변환
+        script_json = json.loads(script_json)
+    except:
+        return False
+
+    # JSON에서 데이터 가져오기
+    return script_json['product']['A']['channel']['naverPaySellerNo'], script_json['product']['A']['productNo']
 
 
 if __name__ == "__main__":
@@ -52,9 +93,15 @@ if __name__ == "__main__":
     if mode == "1":
         r_text = get_thumbnail(url)
     elif mode == "2":
-        r_text = get_detail(url)
+        if get_detail(url):
+            r_text = get_detail(url)
+        else:
+            r_text = get_detail_v2(url)
     elif mode == "3":
-        r_text = get_thumbnail(url) + "\n" + get_detail(url)
+        if get_detail(url):
+            r_text = get_thumbnail(url) + "\n" + get_detail(url)
+        else:
+            r_text = get_thumbnail(url) + "\n" + get_detail_v2(url)
 
     f = open(url.split("/")[5].split("?")[0] + ".txt", "w")
     f.write(r_text)
