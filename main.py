18a1cd8a458c1a90bc6aee3b5f53052870f6b979
unh6uoj <fpdlxms159@gmail.com>
2022-04-09 17:58:31 +0900

    add : crawl all category

--- main.py
+++ main.py
@@ -6,7 +6,10 @@ import json
 def get_thumbnail(url):
     r = requests.get(url)
     soup = BeautifulSoup(r.text, "html.parser")
-    thumbnails = soup.find(class_="_2Yq5J2HeBn").find_all("img")
+    try:
+        thumbnails = soup.find(class_="_2Yq5J2HeBn").find_all("img")
+    except:
+        thumbnails = soup.find(class_="_23RpOU6xpc").find_all("img")
 
     r_text = "썸네일\n"
     try:
@@ -24,10 +27,10 @@ def get_detail(url):
     r = requests.get(
         f"https://smartstore.naver.com/i/v1/products/{code}/contents/-1/PC"
     )
-    soup = BeautifulSoup(str(json.loads(r.text)["renderContent"]), "html.parser")
 
-    r_text = "상세페이지\n"
     try:
+        soup = BeautifulSoup(str(json.loads(r.text)["renderContent"]), "html.parser")
+        r_text = "상세페이지\n"
         main_contents = soup.find(class_="se-main-container")
         for img in main_contents.find_all("img"):
             if img.attrs["class"][0] == "se-image-resource":
@@ -60,8 +63,8 @@ def get_detail_v2(url):
 
 # 기타 상품번호 추출
 def get_etc_no(url):
-    req = requests.get(url)
-    html = req.text
+    response = requests.get(url)
+    html = response.text
     soup = BeautifulSoup(html, 'html.parser')
     try:
         # 받은 url에서 json추출
@@ -80,29 +83,68 @@ def get_etc_no(url):
     # JSON에서 데이터 가져오기
     return script_json['product']['A']['channel']['naverPaySellerNo'], script_json['product']['A']['productNo']
 
+def get_products_by_all_product_url(all_product_url):   # 스마트 스토어 전체상품 url 입력 시 전체 상품 가져옴
+    # all_product_url = all_product_url.replace("size=40", "size=80")
+    all_product_url += "&size=80"
+    response = requests.get(all_product_url)
+    html = response.text
+    soup = BeautifulSoup(html, 'html.parser')
+
+    products = soup.find_all(class_='-qHwcFXhj0')
+
+    results = []
+    for product in products:
+        results.append({
+            "title": product.find('strong').text,
+            "url": f"https://smartstore.naver.com{product.find('a').attrs['href']}"
+        })
+
+    return results
 
 if __name__ == "__main__":
     print("썸네일만 가져오려면 1, 상세페이지만 가져오려면 2, 모두 가져오려면 3을 입력 해주세요")
     mode = input()
 
+    
+
+    print("단일 상품을 가져오시려면 1번, 전체 상품 목록에서 가져오시려면 2번을 눌러주세요")
+    is_all = input()
+
     print("url을 입력해주세요")
-    url = input()
-    url = url.split("?")[0]
+    if is_all == "1":
+        url = input()
+        url = url.split("?")[0]
 
-    r_text = ""
-    if mode == "1":
-        r_text = get_thumbnail(url)
-    elif mode == "2":
-        if get_detail(url):
-            r_text = get_detail(url)
-        else:
-            r_text = get_detail_v2(url)
-    elif mode == "3":
-        if get_detail(url):
-            r_text = get_thumbnail(url) + "\n" + get_detail(url)
-        else:
-            r_text = get_thumbnail(url) + "\n" + get_detail_v2(url)
+        r_text = ""
+        if mode == "1":
+            r_text = get_thumbnail(url)
+        elif mode == "2":
+            if get_detail(url):
+                r_text = get_detail(url)
+            else:
+                r_text = get_detail_v2(url)
+        elif mode == "3":
+            if get_detail(url):
+                r_text = get_thumbnail(url) + "\n" + get_detail(url)
+            else:
+                r_text = get_thumbnail(url) + "\n" + get_detail_v2(url)
 
-    f = open(url.split("/")[5].split("?")[0] + ".txt", "w")
-    f.write(r_text)
-    f.close()
+        f = open(url.split("/")[5].split("?")[0] + ".txt", "w")
+        f.write(r_text)
+        f.close()
+
+    elif is_all == "2":
+        url = input()
+
+        products = get_products_by_all_product_url(url)
+        
+        for product in products:
+            f = open(f"{product['title']}.txt", 'w')
+            r_text = ""
+            if get_detail(url):
+                r_text = get_thumbnail(product["url"]) + "\n" + get_detail(product["url"])
+            else:
+                r_text = get_thumbnail(product["url"]) + "\n" + get_detail_v2(product["url"])
+            
+            f.write(r_text)
+            f.close()
